ec42ef0dec011823d0d9e4bbe0a16573d8bbcd3b
gabrielbarboza-crypto <gabrielbarbozalicnerski@gmail.com>
2025-10-23 01:04:03 +0000

    aula dia 22/10    par e imprar/permitido ou não, e iniciando atividade

---
+++ Aulas/turista/app.js
@@ -0,0 +1,20 @@
+const lugares = ['praia', 'montanha', 'lagoa']
+const mensagem = `
+        -- ROTEIRO VIAGENS --
+        lugares: ${lugares}
+        -------------------`
+alert(mensagem) //fake menu
+const EscolhaUsuario = prompt('pra onde deseja viajar? ')
+if (EscolhaUsuario == null || EscolhaUsuario == '') {
+    alert('por favor digite um valor da lista de viagem!')
+} else {
+    if (lugares.includes(EscolhaUsuario)) {
+        if (EscolhaUsuario == 'praia') {
+            //alert('praia!!')
+            window.location.href = './praia.html'
+        } else if (EscolhaUsuario == 'montanha') {
+            //alert('montanha...!!')
+            window.location.href = './montanha.html'
+        }
+    }
+}
